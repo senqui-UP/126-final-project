f20772d405d989bd65597f42383b2fa1bb6f0389
julia louise <jmcontreras3@up.edu.ph>
2025-05-17 13:37:33 +0800

    Squashed commit of the following:
    commit dc05074f4a484738b0cfdc00fc7aee5446b98233
    Author: julia louise <jmcontreras3@up.edu.ph>
    Date:   Sat May 17 13:29:05 2025 +0800
    
        update login + signup
    
    commit d37a46be8df25274c09ec90e11b2315af2a8fecf
    Author: julia louise <jmcontreras3@up.edu.ph>
    Date:   Sat May 17 13:04:15 2025 +0800
    
        updated signup + login
    
    commit 26b25e18e37e79389cb6c407394fa2350d64f8d7
    Author: Hansen Quindao <hcquindao@up.edu.ph>
    Date:   Sat May 17 12:36:20 2025 +0800
    
        Squashed commit of the following:
    
        commit 2fab3fb6bdd83d95ffce1341f432bb97ec9744c9
        Merge: 54adb1a 28ba1a0
        Author: delrosario-nina <nedelrosario@up.edu.ph>
        Date:   Sat May 17 12:03:17 2025 +0800
    
            Merge branch 'Sprint-2' of https://github.com/senqui-UP/126-final-project into Sprint-2
    
        commit 54adb1a8e5ccf268f1e9ea06066ff662e6f498c1
        Author: delrosario-nina <nedelrosario@up.edu.ph>
        Date:   Sat May 17 12:03:11 2025 +0800
    
            Update ganaps.html
    
        commit 28ba1a084969adb4d2347aa69e48bf045f9450c3
        Author: julia louise <jmcontreras3@up.edu.ph>
        Date:   Thu May 15 11:42:04 2025 +0800
    
            updated login + signup
    
        commit 4c266b9dd2677897f396b683b4086a102a1e2427
        Merge: 81dd486 ac9b81c
        Author: delrosario-nina <nedelrosario@up.edu.ph>
        Date:   Thu May 15 00:45:48 2025 +0800
    
            Merge branch 'Sprint-2' of https://github.com/senqui-UP/126-final-project into Sprint-2
    
        commit 81dd486cb3eb7c4e47053a1b2ef204549743633d
        Author: delrosario-nina <nedelrosario@up.edu.ph>
        Date:   Thu May 15 00:43:56 2025 +0800
    
            poll
    
    commit 7748570fd384c011567ea647834e04716eaac38e
    Author: Hansen Quindao <hcquindao@up.edu.ph>
    Date:   Tue May 13 16:42:08 2025 +0800
    
        start of sprint 3
    
    commit ac9b81c4bd8cfa8b1b568313592efa7295ee5c4c
    Author: Hansen Quindao <hcquindao@up.edu.ph>
    Date:   Tue May 13 16:30:04 2025 +0800
    
        spring cleaning
    
        - added framework of homepage
        - added basic css for navbar
        - spring cleaning on the other files
           - includes renaming files
        - redid images folder
    
    commit 210ffd6a99e5e89581ecefeef69094a81029b259
    Author: julia louise <jmcontreras3@up.edu.ph>
    Date:   Mon May 12 18:08:22 2025 +0800
    
        login -> homepage
    
    commit d7a9a264850cd311950b0dd2fb3e4058e0b7a39f
    Merge: 0f579bd 978cdb7
    Author: julia louise <jmcontreras3@up.edu.ph>
    Date:   Mon May 12 18:04:46 2025 +0800
    
        Merge branch 'Sprint-2' of https://github.com/senqui-UP/126-final-project into Sprint-2
    
    commit 0f579bd8de5691483a7c05ab3a03e315291c7718
    Author: julia louise <jmcontreras3@up.edu.ph>
    Date:   Mon May 12 18:04:36 2025 +0800
    
        new ver of login + signup
    
    commit 978cdb723cd14ea3fcf90afb8bbda3dba2147706
    Merge: 38f2954 5fad1a1
    Author: delrosario-nina <nedelrosario@up.edu.ph>
    Date:   Sun May 11 18:25:54 2025 +0800
    
        Merge branch 'Sprint-2' of https://github.com/senqui-UP/126-final-project into Sprint-2
    
    commit 38f295455762093465d4ec50b10a14f0ba733bf6
    Author: delrosario-nina <nedelrosario@up.edu.ph>
    Date:   Sun May 11 18:25:48 2025 +0800
    
        freedom wall and ganaps
    
    commit 5fad1a1f57715a18708c8ae6678ada8c371bdab7
    Author: julia louise <jmcontreras3@up.edu.ph>
    Date:   Thu May 8 18:49:17 2025 +0800
    
        removed some comments
    
    commit 5ffc4369dddb0e14f57b69ce1c0e7a626dc973dd
    Author: julia louise <jmcontreras3@up.edu.ph>
    Date:   Thu May 8 18:46:59 2025 +0800
    
        login + signup html, css, w font
    
    commit 8ae43ccbae0cbb817333cc0ac951271c6738d64c
    Author: Hansen Quindao <hcquindao@up.edu.ph>
    Date:   Thu May 1 17:44:22 2025 +0800
    
        very skeletal html
    
        added pages

--- script-ganaps.js
+++ script-ganaps.js
@@ -1,6 +1,6 @@
 
-const swiper1 = new Swiper(".swiper1", {
-    loop: true,
+/*const swiper1 = new Swiper(".swiper1", {  
+  loop: true,
     navigation: {
       nextEl: ".swiper1 .swiper-button-next",
       prevEl: ".swiper1 .swiper-button-prev",
@@ -86,3 +86,58 @@ const swiper4 = new Swiper(".swiper4", {
           slidesPerView: 3
         }
   }});
+*/
+
+  const track = document.querySelector('.swiper-wrapper');
+  const nextBtn = document.querySelector('.next');
+  const prevBtn = document.querySelector('.prev');
+  const cards = document.querySelectorAll('.swiper-slide card');
+  const pagination = document.querySelector('.pagination');
+
+  const cardWidth = cards[0].offsetWidth + 20; // 250px card + 20px gap
+  const cardsPerView = Math.floor(track.offsetWidth / cardWidth);
+  const totalPages = Math.ceil(cards.length / cardsPerView);
+
+  // Generate pagination dots
+  for (let i = 0; i < totalPages; i++) {
+    const dot = document.createElement('div');
+    dot.classList.add('carousel-dot');
+    if (i === 0) dot.classList.add('active');
+    dot.dataset.index = i;
+    pagination.appendChild(dot);
+  }
+
+  const dots = document.querySelectorAll('.carousel-dot');
+
+  function updateActiveDot(index) {
+    dots.forEach(dot => dot.classList.remove('active'));
+    if (dots[index]) dots[index].classList.add('active');
+  }
+
+  let currentIndex = 0;
+
+  nextBtn.addEventListener('click', () => {
+    if (currentIndex < totalPages - 1) {
+      currentIndex++;
+      track.scrollBy({ left: track.offsetWidth, behavior: 'smooth' });
+      updateActiveDot(currentIndex);
+    }
+  });
+
+  prevBtn.addEventListener('click', () => {
+    if (currentIndex > 0) {
+      currentIndex--;
+      track.scrollBy({ left: -track.offsetWidth, behavior: 'smooth' });
+      updateActiveDot(currentIndex);
+    }
+  });
+
+  // Click on pagination dot
+  dots.forEach(dot => {
+    dot.addEventListener('click', () => {
+      const index = parseInt(dot.dataset.index);
+      currentIndex = index;
+      track.scrollTo({ left: index * track.offsetWidth, behavior: 'smooth' });
+      updateActiveDot(currentIndex);
+    });
+  });
---
+++ signup.php
@@ -0,0 +1,88 @@
+<?php
+include 'DBConnector.php';
+
+$error = " ";
+
+if ($_SERVER["REQUEST_METHOD"] === "POST"){
+    $uname = $_POST['uname'];
+    $email = $_POST['email'];
+    $password = $_POST['psw'];
+
+    $checksql = "SELECT * FROM users WHERE email = ?";
+    $stmt = $conn->prepare($checksql);
+    $stmt->bind_param("s", $email);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if ($result->num_rows > 0){
+        $error = "This email is already registered.";
+    } else {
+        $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+
+        $insertSql = "INSERT INTO users (username, email, password) VALUES (?, ?, ?)";
+        $stmt = $conn->prepare($insertSql);
+        $stmt->bind_param("sss", $uname, $email, $hashedPassword);
+
+        if ($stmt->execute()) {
+            echo "<script>alert('Signup successful!'); window.location.href='login.php';</script>";
+        } else {
+            echo "Error: " . $stmt->error;
+        }
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link rel="preload" href="Images/Logo.png" as="image">
+        <link href="https://fonts.googleapis.com/css2?family=Pangolin&display=swap" rel="stylesheet">
+
+        <title>UNCENSORED 23</title>
+        <meta charset = "UTF-8">
+        <meta name = "author" content = "Contreras-DelRosario-Quindao">
+        <link rel="stylesheet" type="text/css" href="styling-login.css">
+        <script src="scripts-signup.js"></script>
+    </head> 
+
+    <body>
+        <div class="wrapper">
+            <div class="left-panel">
+                <img src="Images/Logo.png" alt="logo" width="600px" height="500px">
+            </div>
+
+            <div class="right-panel">
+                <br><br>
+
+                <?php if (!empty(trim($error))): ?>
+                    <p id="error-message" style="color:red; margin-bottom: 1em;"><?= $error ?></p>
+                <?php else: ?>
+                    <p id="error-message" style="display:none;"></p>
+                <?php endif; ?>
+                
+                <form method="POST" action="signUp.php" onsubmit="return checkPassword();">
+                    <label for="username"><b>Username</b></label>
+                    <input type="text" placeholder="Enter Username" name="uname" required>
+                    <br>
+
+                    <label for="email"><b>Email</b></label>
+                    <input type="text" placeholder="Enter Email" name="email" required>
+                    <br>
+                
+                    <label for="password"><b>Password</b></label>
+                    <input type="password" id="password" placeholder="Enter Password" name="psw" required>
+                    <span id="error-password" class="error-message"></span>
+
+                    <label class="terms-label">
+                        <input type="checkbox" required name="terms"> By signing up, you agree to our <a href="https://youtu.be/uHgt8giw1LY?si=PSlNQEjlHODbSJOI" target="_blank">Terms, Privacy Policy & Cookies Policy.</a>
+                    </label>
+                <br>
+                <button type="submit">Sign Up</button>
+                <form>
+    
+                <p class="sign-in"> Already have an account? <a class="link-sign-in" href="login.php">LOGIN</a></p>
+        </div>
+    </body>
+</html>
