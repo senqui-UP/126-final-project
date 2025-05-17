bce72a21cd0ab4c92e60f9fe50b1d7eb523770b8
delrosario-nina <nedelrosario@up.edu.ph>
2025-05-17 23:11:08 +0800

    ganaps and poll

--- script-ganaps.js
+++ script-ganaps.js
@@ -1,88 +1,26 @@
 
-const swiper1 = new Swiper(".swiper1", {
-    loop: true,
-    navigation: {
-      nextEl: ".swiper1 .swiper-button-next",
-      prevEl: ".swiper1 .swiper-button-prev",
-    },
-    pagination: {
-      el: ".swiper1 .swiper-pagination",
-      clickable: true,
-    },
-    breakpoints: {
-        0: {
-          slidesPerView: 1
-        },
-        768: {
-          slidesPerView: 2
-        },
-        1024: {
-          slidesPerView: 3
-        }
-  }});
 
-const swiper2 = new Swiper(".swiper2", {
+document.querySelectorAll('.swiper').forEach((swiperEl, index) => {
+  new Swiper(swiperEl, {
     loop: true,
     navigation: {
-      nextEl: ".swiper2 .swiper-button-next",
-      prevEl: ".swiper2 .swiper-button-prev",
+      nextEl: swiperEl.querySelector('.swiper-button-next'),
+      prevEl: swiperEl.querySelector('.swiper-button-prev'),
     },
     pagination: {
-      el: ".swiper2 .swiper-pagination",
+      el: swiperEl.querySelector('.swiper-pagination'),
       clickable: true,
     },
     breakpoints: {
-        0: {
-          slidesPerView: 1
-        },
-        768: {
-          slidesPerView: 2
-        },
-        1024: {
-          slidesPerView: 3
-        }
-  }});
-
-const swiper3 = new Swiper(".swiper3", {
-    loop: true,
-    navigation: {
-      nextEl: ".swiper3 .swiper-button-next",
-      prevEl: ".swiper3 .swiper-button-prev",
+      0: {
+        slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
     },
-    pagination: {
-      el: ".swiper3 .swiper-pagination",
-      clickable: true,
-    },
-    breakpoints: {
-        0: {
-          slidesPerView: 1
-        },
-        768: {
-          slidesPerView: 2
-        },
-        1024: {
-          slidesPerView: 3
-        }
-  }});
-
-const swiper4 = new Swiper(".swiper4", {
-    loop: true,
-    navigation: {
-      nextEl: ".swiper4 .swiper-button-next",
-      prevEl: ".swiper4 .swiper-button-prev",
-    },
-    pagination: {
-      el: ".swiper4 .swiper-pagination",
-      clickable: true,
-    },
-    breakpoints: {
-        0: {
-          slidesPerView: 1
-        },
-        768: {
-          slidesPerView: 2
-        },
-        1024: {
-          slidesPerView: 3
-        }
-  }});
+  });
+});
