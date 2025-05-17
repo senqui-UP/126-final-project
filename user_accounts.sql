b6ed7e87cdee0eb9bfe56cfbaaa3b97d53613cc7
julia louise <jmcontreras3@up.edu.ph>
2025-05-17 18:12:52 +0800

    update sql

--- user_accounts.sql
+++ user_accounts.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 17, 2025 at 07:02 AM
+-- Generation Time: May 17, 2025 at 12:07 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -20,6 +20,28 @@ SET time_zone = "+00:00";
 --
 -- Database: `user_accounts`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `post`
+--
+
+CREATE TABLE `post` (
+  `postID` int(11) NOT NULL,
+  `content` longtext DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `posting`
+--
+
+CREATE TABLE `posting` (
+  `id` int(11) DEFAULT NULL,
+  `postID` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -46,6 +68,19 @@ INSERT INTO `users` (`id`, `username`, `email`, `password`) VALUES
 --
 
 --
+-- Indexes for table `post`
+--
+ALTER TABLE `post`
+  ADD PRIMARY KEY (`postID`);
+
+--
+-- Indexes for table `posting`
+--
+ALTER TABLE `posting`
+  ADD KEY `id` (`id`),
+  ADD KEY `postID` (`postID`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -57,10 +92,27 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `post`
+--
+ALTER TABLE `post`
+  MODIFY `postID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `posting`
+--
+ALTER TABLE `posting`
+  ADD CONSTRAINT `posting_ibfk_1` FOREIGN KEY (`id`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `posting_ibfk_2` FOREIGN KEY (`postID`) REFERENCES `post` (`postID`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
